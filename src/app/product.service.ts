3005d0daaa490c27ffa45028381bee657815d361
296ELKS <ernest@296elks.ca>
2020-11-05 18:56:20 -0500

    1 binaries uploaded

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class ProductService {
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   getProducts() {
-    return this.http.get("/assets/data/products.json");
+    return [
+      {
+        productId: 1,
+        name: "Phone XL",
+        price: 799,
+        description: "A large phone with one of the best screens"
+      },
+      {
+        productId: 2,
+        name: "Phone Mini",
+        price: 699,
+        description: "A great phone with one of the best cameras"
+      },
+      {
+        productId: 3,
+        name: "Phone Standard",
+        price: 299,
+        description: ""
+      }
+    ];
   }
 
   getProduct(id) {
-    return this.getProducts();
+    return this.getProducts().find(item => (item.productId = id));
   }
 }
