cb6aafd50a598d63980bc4a173752a25e9854042
296ELKS <ernest@296elks.ca>
2020-11-05 16:26:25 -0500

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
 
 @NgModule({
   imports: [
@@ -14,7 +17,8 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
-      { path: "products/:productId", component: ProductDetailsComponent }
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent }
     ])
   ],
   declarations: [
@@ -22,10 +26,11 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponenComponent,
-    ProductDetailsComponentComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [CartService]
 })
 export class AppModule {}
 
